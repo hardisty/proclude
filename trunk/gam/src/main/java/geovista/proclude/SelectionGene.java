35a8c6e51d0454132fba04ba665e64de7ddc4f77
jfconley <jfconley@e99fcd4e-ed4e-0410-826a-5b328e25170d>
2008-06-16 15:25:57 +0000

    Bug fixes for initialization problems.

--- trunk/gam/src/main/java/geovista/proclude/SelectionGene.java
+++ trunk/gam/src/main/java/geovista/proclude/SelectionGene.java
@@ -19,23 +19,31 @@ import java.util.Vector;
  */
 public class SelectionGene extends Gene{
     
+    int[] selection;
+    
     /** Creates a new instance of SelectionGene */
     public SelectionGene(int[] ints, Fitness f) {
         super(0, 0, 0, 0, 0, f);
+        selection = ints;
         count = function.count(this);
         population = function.getPopulation();
         fitness = function.run(this);
-        containedPoints = function.getContainedPoints();        
+        containedPoints = function.getContainedPoints();   
     }
     
     public Shape toShape(){
-        Vector data = function.dataSet;
-        //TODO: turn the Vector into an array of Point2D.Doubles and create a MultiPoint with it.
-        Point2D.Double[] points = new Point2D.Double[data.size()];
-        for (int i = 0; i < data.size(); i++){
-            points[i] = ((DataPoint) data.get(i)).getLocation();
+        if (selection == null){
+            //this should only occur when intializing
+            return new MultiPoint(new Point2D.Double[0]);
+        } else {        
+            Vector data = function.dataSet;
+            //TODO: turn the Vector into an array of Point2D.Doubles and create a MultiPoint with it.
+            Point2D.Double[] points = new Point2D.Double[selection.length];
+            for (int i = 0; i < selection.length; i++){
+                points[i] = ((DataPoint) data.get(selection[i])).getLocation();
+            }
+            return new MultiPoint(points);
         }
-        return new MultiPoint(points);
     }
     
 }
